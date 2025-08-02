78ebf439d8ba580006ce9248fc41efe76ccd4bb5
Emir Yeniçeri <emiryncr@gmail.com>
2025-08-02 20:52:47 +0300

    e2e testing config

--- Part4/blog-list/app.js
+++ Part4/blog-list/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const blogRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const testingRouter = require('./controllers/testing');
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
@@ -30,6 +31,10 @@ app.get('/', (req, res) => {
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
